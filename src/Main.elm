0667311ce9b68e134d7951bd732c2ab0e4312e6d
Paul Venable <pvenable@gmail.com>
2017-05-21 20:37:26 -0700

    Add routing and player edit view

--- src/Main.elm
+++ src/Main.elm
@@ -4,13 +4,19 @@ import Commands exposing (fetchPlayers)
 import Html exposing (program)
 import Models exposing (Model, initialModel)
 import Msgs exposing (Msg)
+import Navigation exposing (Location)
+import Routing
 import Update exposing (update)
 import View exposing (view)
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( initialModel, fetchPlayers )
+init : Location -> ( Model, Cmd Msg )
+init location =
+    let
+        currentRoute =
+            Routing.parseLocation location
+    in
+    ( initialModel currentRoute, fetchPlayers )
 
 
 
@@ -28,7 +34,7 @@ subscriptions model =
 
 main : Program Never Model Msg
 main =
-    program
+    Navigation.program Msgs.OnLocationChange
         { init = init
         , view = view
         , update = update
